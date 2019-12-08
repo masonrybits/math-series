919804fdb2d3787f7b7bb178c16e17ae2c49cfd1
Ting Luo <tin831@gmail.com>
2019-12-08 14:37:53 -0800

    changed functions to return the proper value in series & tested

--- README.md
+++ README.md
@@ -13,9 +13,12 @@ The user is required to run the application by running the source codes in Pytho
 This application is built using Python.
 
 ## API
-n/a
+fibonacci(n) returns the nth value in the fibonacci series.
+lucas(n) returns the nth value in the lucas numbers.
+sum_series(n, a=0, b=1) returns the nth value in the fibonacci series, and returns nth value in the lucas numbers when a=2 & b=1.
 
 ## Change Log
 12-04-2019 6:30 AM - Added fibonacci funtion.
 12-04-2019 6:45 AM - Added lucas functions.
 12-04-2019 7:00 AM - Added sum series function.
+12-08-2019 2:35 PM - Fixed bugs.
--- test_series.py
+++ test_series.py
@@ -2,8 +2,13 @@ from series import fibonacci, lucas, sum_series
 
 # testing for the fibonacci series
 
-def test_fib_one():
+def test_fib_zero():
     expected = 0
+    actual = fibonacci(0)
+    assert actual == expected
+
+def test_fib_one():
+    expected = 1
     actual = fibonacci(1)
     assert actual == expected
 
@@ -13,85 +18,89 @@ def test_fib_two():
     assert actual == expected
 
 def test_fib_three():
-    expected = 1
+    expected = 2
     actual = fibonacci(3)
     assert actual == expected
 
 def test_fib_four():
-    expected = 2
+    expected = 3
     actual = fibonacci(4)
     assert actual == expected
 
 def test_fib_five():
-    expected = 3
+    expected = 5
     actual = fibonacci(5)
     assert actual == expected
 
 def test_fib_six():
-    expected = 5
+    expected = 8
     actual = fibonacci(6)
     assert actual == expected
 
 # testing for the lucas series
+def test_lucas_zero():
+    expected = 2
+    actual = lucas(0)
+    assert actual == expected
 
 def test_lucas_one():
-    expected = 2
+    expected = 1
     actual = lucas(1)
     assert actual == expected
 
 def test_lucas_two():
-    expected = 1
+    expected = 3
     actual = lucas(2)
     assert actual == expected
 
 def test_lucas_three():
-    expected = 3
+    expected = 4
     actual = lucas(3)
     assert actual == expected
 
 def test_lucas_four():
-    expected = 4
+    expected = 7
     actual = lucas(4)
     assert actual == expected
 
 def test_lucas_five():
-    expected = 7
+    expected = 11
     actual = lucas(5)
     assert actual == expected
 
 def test_lucas_six():
-    expected = 11
+    expected = 18
     actual = lucas(6)
     assert actual == expected
 
 # testing for the sum series
 
 def test_sum_fib_one():
-    expected = 0
+    expected = 1
     actual = sum_series(1)
     assert actual == expected
 
 def test_sum_fib_three():
-    expected = 1
+    expected = 2
     actual = sum_series(3)
     assert actual == expected
 
 def test_sum_fib_six():
-    expected = 5
+    expected = 8
     actual = sum_series(6)
     assert actual == expected
 
 def test_sum_lucas_one():
-    expected = 2
+    expected = 1
     actual = sum_series(1, 2, 1)
     assert actual == expected
 
 def test_sum_lucas_three():
-    expected = 3
+    expected = 4
     actual = sum_series(3, 2, 1)
     assert actual == expected
 
 def test_sum_lucas_six():
-    expected = 11
+    expected = 18
     actual = sum_series(6, 2, 1)
     assert actual == expected
